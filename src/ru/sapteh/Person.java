9ac5d2fb432bca7626ae871fd2e2100ae3bc4e3f
Danil Stozhkov <lekseih2000@mail.ru>
2020-09-29 11:58:36 +0300

    PERson

--- src/ru/sapteh/Person.java
+++ src/ru/sapteh/Person.java
@@ -3,6 +3,22 @@ package ru.sapteh;
 public class Person {
     private String name;
     private int age;
+    private int date;
+    public Person(String name, int age, int date){
+        this.name = name;
+        this.age = age;
+        this.date = date;
+    }
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Person(){
+
+    }
+    public Person(String name){
+        this.name = name;
+    }
     public String getName(){
         return name;
     }
@@ -14,5 +30,16 @@ public class Person {
     }
     public void setAge(int age){
         this.age = age;
+    }
+    public int getDate(){
+        return date;
+    }
+    public void setDate(){
+        this.date = date;
+    }
+    @Override
+    public String toString(){
+       return  "Name: " + getName()+ "\n" +
+        "Age: " + getAge()+ "\n" + "Date: " + getDate();
     }
 }
--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -1,7 +1,12 @@
 package ru.sapteh;
 
 public class Program {
-    public static void main(String[] args){ 
-        System.out.println("Daniil krasava");
+    public static void main(String[] args) {
+        Person person = new Person("Mikhail", 19, 2001);
+        Person person1 = new Person("Alexey", 32, 1987);
+        Person person2 = new Person("Maxim", 12, 2008);
+        System.out.println(person);
+        System.out.println(person1);
+        System.out.println(person2);
     }
 }
